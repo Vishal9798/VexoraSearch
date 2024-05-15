f92f741663685379c1a4709a4928b3c31c19b556
Vishal9798 <vishal.p12344@gmail.com>
2024-05-15 11:14:23 +0530

    chore: Refactor PaginationButton and SearchBox components

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,50 +1,10 @@
-// 'use client'
-// import React from 'react'
-// import { useSearchParams, useRouter } from 'next/navigation';
-// import {RxCross2} from 'react-icons/rx'
-// import { AiOutlineSearch } from 'react-icons/ai'
-// import { useState } from 'react';
-
-
-// function SearchBox() {
-//   const searchParams = useSearchParams();
-//   const router = useRouter();
-//   const searchTerm = searchParams.get('searchTerm');
-//   const [term, setTerm] = useState(searchTerm || '');
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     if (!term.trim()) return;
-//     router.push(`/search/web?searchTerm=${term}`);
-//   };
-//   return (
-//     <form  onSubmit={handleSubmit}
-//     className='flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center'>
-//       <input 
-//       type="text"
-//       className='w-full focus:outline-none'
-//       value={term}
-//       onChange={(e) => setTerm(e.target.value)}
-//       />
-//       <RxCross2 onClick={() => setTerm('')} 
-//       className='text-2xl text-gray-500 cursor-pointer sm:mr-2'
-//        />
-//       <AiOutlineSearch onClick={handleSubmit}
-//         className='text-3xl hidden sm:inline-flex text-blue-500 border-l-2  border-gray-300 mr-0 pl-2
-//       cursor-pointer'/>
-//     </form>
-//   )
-// }
-
-// export default SearchBox
-
-
-
-'use client';
-import React, { Suspense, useState } from 'react';
+'use client'
+import React from 'react'
 import { useSearchParams, useRouter } from 'next/navigation';
-import { RxCross2 } from 'react-icons/rx';
-import { AiOutlineSearch } from 'react-icons/ai';
+import {RxCross2} from 'react-icons/rx'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { useState } from 'react';
+
 
 function SearchBox() {
   const searchParams = useSearchParams();
@@ -57,36 +17,25 @@ function SearchBox() {
     if (!term.trim()) return;
     router.push(`/search/web?searchTerm=${term}`);
   };
-
   return (
-    <form 
-      onSubmit={handleSubmit}
-      className='flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center'
-    >
+    <form  onSubmit={handleSubmit}
+    className='flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center'>
       <input 
-        type="text"
-        className='w-full focus:outline-none'
-        value={term}
-        onChange={(e) => setTerm(e.target.value)}
+      type="text"
+      className='w-full focus:outline-none'
+      value={term}
+      onChange={(e) => setTerm(e.target.value)}
       />
-      <RxCross2 
-        onClick={() => setTerm('')} 
-        className='text-2xl text-gray-500 cursor-pointer sm:mr-2'
-      />
-      <AiOutlineSearch 
-        onClick={handleSubmit}
-        className='text-3xl hidden sm:inline-flex text-blue-500 border-l-2 border-gray-300 mr-0 pl-2 cursor-pointer'
-      />
+      <RxCross2 onClick={() => setTerm('')} 
+      className='text-2xl text-gray-500 cursor-pointer sm:mr-2'
+       />
+      <AiOutlineSearch onClick={handleSubmit}
+        className='text-3xl hidden sm:inline-flex text-blue-500 border-l-2  border-gray-300 mr-0 pl-2
+      cursor-pointer'/>
     </form>
-  );
+  )
 }
 
-function WrappedSearchBox() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <SearchBox />
-    </Suspense>
-  );
-}
+export default SearchBox
 
-export default WrappedSearchBox;
+
--- src/components/SearchHeaderOptions.jsx
+++ src/components/SearchHeaderOptions.jsx
@@ -1,68 +1,24 @@
-// 'use client';
-// import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-// import { AiOutlineCamera, AiOutlineSearch } from 'react-icons/ai';
-
-// export default function SearchHeaderOptions() {
-//   const router = useRouter();
-//   const searchParams = useSearchParams();
-//   const searchTerm = searchParams.get('searchTerm');
-//   const pathname = usePathname();
-//   const selectTab = (tab) => {
-//     router.push(
-//       `/search/${tab === 'Images' ? 'image' : 'web'}?searchTerm=${searchTerm}`
-//     );
-//   };
-//   return (
-//     <div className='flex space-x-2 select-none border-b w-full justify-center lg:justify-start lg:pl-52 text-gray-700 text-sm'>
-//       <div onClick={()=>selectTab("All")} className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${pathname === '/search/web' && '!text-blue-600 !border-blue-600'}`}>
-//         <AiOutlineSearch className='text-md' />
-//         <p>All</p>
-//       </div>
-//       <div onClick={()=>selectTab("Images")} className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${pathname === '/search/image' && '!text-blue-600 !border-blue-600'}`}>
-//         <AiOutlineCamera className='text-md' />
-//         <p>Images</p>
-//       </div>
-//     </div>
-//   );
-// }
-
-
-
-
 'use client';
-import React, { Suspense } from 'react';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { AiOutlineCamera, AiOutlineSearch } from 'react-icons/ai';
 
-function SearchHeaderOptions() {
+export default function SearchHeaderOptions() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const searchTerm = searchParams.get('searchTerm');
   const pathname = usePathname();
-
   const selectTab = (tab) => {
     router.push(
       `/search/${tab === 'Images' ? 'image' : 'web'}?searchTerm=${searchTerm}`
     );
   };
-
   return (
     <div className='flex space-x-2 select-none border-b w-full justify-center lg:justify-start lg:pl-52 text-gray-700 text-sm'>
-      <div
-        onClick={() => selectTab('All')}
-        className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${
-          pathname === '/search/web' && '!text-blue-600 !border-blue-600'
-        }`}
-      >
+      <div onClick={()=>selectTab("All")} className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${pathname === '/search/web' && '!text-blue-600 !border-blue-600'}`}>
         <AiOutlineSearch className='text-md' />
         <p>All</p>
       </div>
-      <div
-        onClick={() => selectTab('Images')}
-        className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${
-          pathname === '/search/image' && '!text-blue-600 !border-blue-600'
-        }`}
-      >
+      <div onClick={()=>selectTab("Images")} className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${pathname === '/search/image' && '!text-blue-600 !border-blue-600'}`}>
         <AiOutlineCamera className='text-md' />
         <p>Images</p>
       </div>
@@ -70,11 +26,5 @@ function SearchHeaderOptions() {
   );
 }
 
-// Create a wrapper component with Suspense
-export default function WrappedSearchHeaderOptions() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <SearchHeaderOptions />
-    </Suspense>
-  );
-}
+
+
